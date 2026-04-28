c89e7a492a494e29a4cc7e96193b22f665874a4d
Danil Khaliulin <danielooneilie@gmail.com>
2026-04-28 12:46:31 +0700

    fix: import CDEK PrimeReact theme styles (SCSS)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '../primereact-master/src/style.scss'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
